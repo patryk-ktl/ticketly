b27fa8ca1a201dc1978aa698d71fc9356e07d8e8
ktlpatryk <ktlpatryk@gmail.com>
2026-08-06 19:06:11 +0200

    feat: added the ticketing-service module, configured datasource & added health check endpoint

---
+++ ticketing-service/src/test/java/io/github/patrykktl/ticketly/ticketingservice/TicketingServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package io.github.patrykktl.ticketly.ticketingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+        //empty
+    }
+
+}
