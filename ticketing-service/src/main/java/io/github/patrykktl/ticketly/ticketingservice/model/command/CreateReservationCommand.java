f478b2523ee0572dbd2f2d494e9896467ec33220
ktlpatryk <ktlpatryk@gmail.com>
2026-08-08 01:05:07 +0200

    fix: added constructors to CreateReservationCommand for proper json deserialization

--- ticketing-service/src/main/java/io/github/patrykktl/ticketly/ticketingservice/model/command/CreateReservationCommand.java
+++ ticketing-service/src/main/java/io/github/patrykktl/ticketly/ticketingservice/model/command/CreateReservationCommand.java
@@ -2,13 +2,18 @@ package io.github.patrykktl.ticketly.ticketingservice.model.command;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Getter
 @Builder
 public class CreateReservationCommand {
@@ -18,7 +23,7 @@ public class CreateReservationCommand {
     private Integer eventId;
 
     @Email(message = "INVALID_EMAIL")
-    @NotNull(message = "NULL_VALUE")
+    @NotBlank(message = "BLANK_VALUE")
     private String customerEmail;
 
     @Min(1)
