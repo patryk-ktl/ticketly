a3965317b1be13b256227b726af0c3fd9f07125b
ktlpatryk <ktlpatryk@gmail.com>
2026-08-13 17:00:17 +0200

    ci: add workflow and break test to verify PR status

--- ticketing-service/src/test/java/io/github/patrykktl/ticketly/ticketingservice/TicketingServiceApplicationTests.java
+++ ticketing-service/src/test/java/io/github/patrykktl/ticketly/ticketingservice/TicketingServiceApplicationTests.java
@@ -2,8 +2,10 @@ package io.github.patrykktl.ticketly.ticketingservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class TicketingServiceApplicationTests {
 
     @Test
