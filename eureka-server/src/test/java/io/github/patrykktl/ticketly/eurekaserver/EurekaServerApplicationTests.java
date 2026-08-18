1aa9f7792d471c04aae159c5dd56aaf130fe1709
ktlpatryk <ktlpatryk@gmail.com>
2026-08-18 16:23:59 +0200

    feat: added and configured a eureka server module that discovers instances of ticketing-service and payment-service

---
+++ eureka-server/src/test/java/io/github/patrykktl/ticketly/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,14 @@
+package io.github.patrykktl.ticketly.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+        //empty
+    }
+
+}
