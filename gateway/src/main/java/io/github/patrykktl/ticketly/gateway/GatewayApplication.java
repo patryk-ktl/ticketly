f015eb1e54d21a1ff98e438cd41e33e2c29d7f3b
ktlpatryk <ktlpatryk@gmail.com>
2026-08-19 14:45:55 +0200

    feat: added gateway with load balancing with routes to ticketing-service and payment-service

---
+++ gateway/src/main/java/io/github/patrykktl/ticketly/gateway/GatewayApplication.java
@@ -0,0 +1,13 @@
+package io.github.patrykktl.ticketly.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+}
--- ticketing-service/src/main/java/io/github/patrykktl/ticketly/ticketingservice/client/PaymentClient.java
+++ ticketing-service/src/main/java/io/github/patrykktl/ticketly/ticketingservice/client/PaymentClient.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 @FeignClient(name = "payment-service", fallback = PaymentClientFallback.class)
 public interface PaymentClient {
 
-    @PostMapping("/payments")
+    @PostMapping("/api/payments")
     PaymentResponse charge(@RequestBody PaymentRequest request);
 }
